e2fb48c607e1da2391685e39ad8c8e2deecdef05
CtrAtlDelete <kvasovski34@gmail.com>
2022-01-26 16:34:43 +0300

    Add .proto files

--- CourseProject/CourseProject.Client/Program.cs
+++ CourseProject/CourseProject.Client/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Logging.Abstractions;
 
-Console.WriteLine("Hello, World!");
+namespace CourseProject.Client
+{
+    internal static class Programm
+    {
+        public static async Task Main()
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress(
+                "http://localhost:18081",
+                new GrpcChannelOptions()
+                {
+                    Credentials = ChannelCredentials.Insecure,
+                    LoggerFactory = new NullLoggerFactory()
+                }
+            );
+            // var mss = new MatrixSumService.MatrixSumServiceClient(channel);
+        }
+
+        private static async Task Input()
+        {
+            //input Console or File
+            var tasks = new Dictionary<int, Task>();
+            await Task.WhenAll(tasks.Values);
+        }
+    }
+};
--- CourseProject/CourseProject.Worker/Program.cs
+++ CourseProject/CourseProject.Worker/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Serilog;
+
+namespace CourseProject.Worker
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello im worker");
+            Log.Logger = new LoggerConfiguration() //Create logger
+                .MinimumLevel.Information()
+                .WriteTo.Console().CreateLogger();
+            Log.Information("Worker is start");
+            var bootstrapServers = Environment.GetEnvironmentVariable("MSS_KAFKA_BOOTSTRAP_SERVERS") 
+                                   ?? "localhost:29092";
+            var groupId = Environment.GetEnvironmentVariable("MSS_KAFKA_GROUP_ID") ?? "mss.workers10";
+            var rqTopic = Environment.GetEnvironmentVariable("MSS_KAFKA_TOPIC_RQ") ?? "mss.worker.rq";
+            var rsTopic = Environment.GetEnvironmentVariable("MSS_KAFKA_TOPIC_RS") ?? "mss.worker.rs";
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = groupId,
+                AutoOffsetReset = AutoOffsetReset.Latest
+            };
+        //     using var consumer = new ConsumerBuilder<Null, WorkerRq>(consumerConfig)
+        //         .SetValueDeserializer(new ProtoDeserializer<WorkerRq>())
+        //         .Build();
+        //
+        //     var producerConfig = new ProducerConfig()
+        //     {
+        //         BootstrapServers = bootstrapServers
+        //     };
+        //     using var producer = new ProducerBuilder<Null, WorkerRs>(producerConfig)
+        //         .SetValueSerializer(new ProtoSerializer<WorkerRs>())
+        //         .Build();
+        //     Log.Information("Serving...");
+        //     Service.Serve(rqTopic, consumer, rsTopic, producer);
+        //     Log.CloseAndFlush();
+        }
+    }
+};
+
