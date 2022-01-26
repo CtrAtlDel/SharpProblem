5ede5bb195008291cec0e8f936374c42ed02e9af
CtrAtlDelete <kvasovski34@gmail.com>
2022-01-26 21:22:44 +0300

    half done

--- CourseProject/CourseProject.Worker/Algorithms.cs
+++ CourseProject/CourseProject.Worker/Algorithms.cs
@@ -1,6 +1,9 @@
-namespace CourseProject.Worker;
-
-public class Algorithms
+namespace CourseProject.Worker
 {
-    
+    public static class Algorithms
+    {
+        // public static Matrix Sum(PairMatrix ms)
+        // {
+        // }
+    }
 }
