2a0de3ea67ff52f629c80fb8dc163d6bc80ba8c1
CtrAtlDelete <kvasovski34@gmail.com>
2022-05-10 14:34:34 +0300

    Add lsn2 with JWT token

---
+++ EnityFrameworkLessons/Lsn2/Lsn2/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Lsn2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Lsn2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : Controller
+{
+    [HttpPost, Route("login")]
+    public IActionResult Login(LoginDTO loginDTO)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(loginDTO.UserName) ||
+                string.IsNullOrEmpty(loginDTO.Password))
+                return BadRequest("Username and/or Password not specified");
+            if (loginDTO.UserName.Equals("joydip") &&
+                loginDTO.Password.Equals("joydip123"))
+            {
+                var secretKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes("thisisasecretkey@123"));
+                var signinCredentials = new SigningCredentials
+                    (secretKey, SecurityAlgorithms.HmacSha256);
+                var jwtSecurityToken = new JwtSecurityToken(
+                    issuer: "ABCXYZ",
+                    audience: "http://localhost:51398",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(10),
+                    signingCredentials: signinCredentials
+                );
+                Ok(new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken));
+            }
+        }
+        catch
+        {
+            return BadRequest
+                ("An error occurred in generating the token");
+        }
+
+        return Unauthorized();
+    }
+}
---
+++ EnityFrameworkLessons/Lsn2/Lsn2/Models/LoginDTO.cs
@@ -0,0 +1,7 @@
+namespace Lsn2.Models;
+
+public class LoginDTO
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
